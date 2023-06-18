7617e45cd2eb8372ac1a46f9141587f68fad22b0
Tyler Candon <tylermcandon@gmail.com>
2023-06-18 15:47:04 -0500

    Add messageGenerator & printMessage functions

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-let array1 = ["phrases"]
+let array1 = ["phrase1", "phrase2", "phrase3"]
 
-let array2 = ["phrases"]
+let array2 = ["monkey1", "monkey2", "monkey3"]
 
-let array3 = ["phrases"]
+let array3 = ["horse1", "horse2", "horse3"]
+
+let arrays = [array1, array2, array3]
+
+
+const messageGenerator = arr => {
+    let randPhrase = []
+    for (let i = 0; i < arr.length; i++) {
+        let x = Math.floor(Math.random() * arr[i].length);
+        randPhrase.push(arr[i][x]);
+    }
+    return randPhrase;
+}
+
+const printMessage = arr => {
+    console.log(arr[0]);
+    console.log(arr[1]);
+    console.log(arr[2]);
+}
+
+printMessage(messageGenerator(arrays))
 
