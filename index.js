f14bf82fe2b5b047e9799073fa74280a18012803
Tyler Candon <tylermcandon@gmail.com>
2023-06-18 16:07:28 -0500

    Add ASCII phrases

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-let array1 = ["Where did you come from?", "You have something in your teeth.", "Cotton Eye Joe", "Shall I compare thee to a summer's day?", "Your United States Postal Service"]
+let array1 = ["Where did you come from?", "You have something in your teeth.", "Cotton Eye Joe", "Shall I compare thee to a summer's day?", "Your United States Postal Service", "*=*"]
 
-let array2 = ["It's man's job to ask why why why.", "ACK ACK ACK", "The silo is always one problem away from disaster.", "I need a drink.", "Where are my PAAAAAANTS?"]
+let array2 = ["It's man's job to ask why why why.", "ACK ACK ACK", "The silo is always one problem away from disaster.", "I need a drink.", "Where are my PAAAAAANTS?", "o7"]
 
-let array3 = ["New boot scootin.", "Did you betray me?", "This just reminds people of that.", "Howdy hey.", "raucous laughter"]
+let array3 = ["New boot scootin.", "Did you betray me?", "This just reminds people of that.", "Howdy hey.", "raucous laughter", ":(", ":)"]
 
 let arrays = [array1, array2, array3]
 
